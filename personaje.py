e8ea676b5761392bdf3b083f5728aa90b23f84c8
JavieraVega17 <Javieravega@insucoabg.cl>
2025-10-29 09:06:58 -0300

    Update personaje.py

--- personaje.py
+++ personaje.py
@@ -3,17 +3,25 @@ import constantes
 
 class Personaje():
     def __init__(self, x, y, image):
+        self.flip = False
         self.image = image 
         self.forma = pygame.Rect(0, 0, constantes.ANCHO_PERSONAJE, constantes.ALTO_PERSONAJE)
 
         self.forma.center = (x, y)
 
     def movimiento(self, delta_x, delta_y):
+            
+            if delta_x < 0:
+                 self.flip = True
+            if delta_y > 0:
+                 self.flip = False
+
             self.forma.x = self.forma.x + delta_x
             self.forma.y = self.forma.y + delta_y
 
     def dibujar(self, interfaz):
-        interfaz.blit(self.image, self.forma)
+        imagen_flip = pygame.transform.flip(self.image, self.flip, flip_y= False)
+        interfaz.blit(imagen_flip, self.forma)
         #pygame.draw.rect(interfaz, constantes.COLOR_PERSONAJE, self.forma, width=1)
 
     
